a5db1a5d08030ef1382dad024718d539036f0e02
1blac <72660276+1blac@users.noreply.github.com>
2021-05-18 12:06:36 +0300

    Add files via upload

---
+++ UnitTest_6.7/UnitTest_6.7.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Laboratory_6.7/Array.h"
+#include "../Laboratory_6.7/Array.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest67
+{
+	TEST_CLASS(UnitTest67)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int size = 3;
+			Array res = Array(size);
+			for (int i = 0; i < size; i++)
+			{
+				res[i] = 1.1;
+			}
+			Assert::AreEqual(3.3, res.Sum(), 0.1);
+		}
+	};
+}
